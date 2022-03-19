0442d09533f7c63c2cfc085dcc45baa260bc05aa
Casey Cochran <Casey3362@yahoo.com>
2022-03-19 10:34:55 -0700

    create post styling added

--- frontend/src/components/CreatePost/CreatePost.js
+++ frontend/src/components/CreatePost/CreatePost.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createUserPost } from "../../store/posts";
+import { BiImage } from 'react-icons/bi';
 import "./CreatePost.css";
 
 const CreatePost = ({ closeModal }) => {
@@ -13,6 +14,20 @@ const CreatePost = ({ closeModal }) => {
   const [caption, setCaption] = useState("");
   const [errors, setErrors] = useState([]);
   const [openForm, setOpenForm] = useState(false);
+
+  let buttonClass = ''
+  let createImageClass = ''
+ if(image){
+   buttonClass = 'post-comment'
+ }else {
+  buttonClass = 'post-comment-disabled'
+}
+if(caption){
+  createImageClass = 'post-comment'
+}else {
+  createImageClass = 'post-comment-disabled'
+}
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,44 +51,49 @@ const CreatePost = ({ closeModal }) => {
   };
 
   return (
-    <div>
+    <div >
       {openForm ? (
-        <div>
-          <img id="testimg" src={image} />
-          <form onSubmit={handleSubmit}>
+        <div className="create-post-cont">
+          <img id="add-img" src={image} />
+          <form className="create-post-form" onSubmit={handleSubmit}>
             <ul>
               {errors?.map((error, index) => {
                return <li key={index}>{error}</li>;
               })}
             </ul>
             <div>
-              <label htmlFor="caption">Caption</label>
               <input
                 onChange={(e) => setCaption(e.target.value)}
                 value={caption}
                 type="text"
                 id="caption"
+                placeholder="Add caption"
+                className="create-post-input"
               />
             </div>
-            <button type="submit">Add New Post</button>
+            <button onClick={() => setOpenForm(true)} disabled={caption ? false : true} className={createImageClass} type='submit'>Add image</button>
+ 
           </form>
          {errors.length > 0 && <button onClick={() => setOpenForm(false)}>Go back to add image</button> }
         </div>
       ) : (
-        <div>
-          <h3>Create new post</h3>
-          <form>
+        <div className="create-post-cont">
+          <h3 className="create-post-title">Create new post</h3>
+          {/* <BiImage className="create-post-icon"/> */}
+          <img className="create-post-icon" src='https://www.cera.org.au/wp-content/uploads/2021/06/placeholder-images-image_large.png' />
+          <form className="create-post-form">
             <div>
-              <label htmlFor="image">Image link</label>
               <input
                 onChange={(e) => setImage(e.target.value)}
                 value={image}
                 required
                 type="url"
                 id="image"
+                placeholder="Add image url"
+                className="create-post-input"
               />
             </div>
-            <button onClick={() => setOpenForm(true)}>Add Image</button>
+            <button onClick={() => setOpenForm(true)} disabled={image ? false : true} className={buttonClass} type='submit'>Add image</button>
           </form>
         </div>
       )}
