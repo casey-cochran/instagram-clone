fdaa9876cad6f2cdc74e4efd0d6251a8166f1b6b
Casey Cochran <Casey3362@yahoo.com>
2022-04-22 20:24:12 -0700

    basic search route written

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const {Op} = require('sequelize')
+const {Op} = require('sequelize');
+const {User} = require('../../db/models');
 
 
 
-
+router.post('', asyncHandler(async(req,res) => {
+    const {searchVal} = req.body;
+    const value = await User.findAll({
+        where: {
+            username: {
+                [Op.like]: '%' + searchVal + '%'
+            }
+        }
+    })
+    res.json(value);
+}));
 
 
 
