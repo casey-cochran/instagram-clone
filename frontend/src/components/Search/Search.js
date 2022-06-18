c78d2e11b27128f6b73b10ab6fc498556ad407d7
Casey Cochran <Casey3362@yahoo.com>
2022-06-18 16:57:12 -0700

    working on arrow key function for list iteration

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -11,7 +11,16 @@ const Search = () => {
   const history = useHistory();
   const [searchVal, setSearchVal] = useState('');
   const [menu, setMenu] = useState(false);
+  const [active, setActive] = useState(0)
   const searchResults = useSelector((state) => Object.values(state.searchReducer.Search))
+
+  const keyDown = (e) => {
+    if(e.key === 38 && searchResults.length > 0){
+      setActive(active - 1)
+    }else if(e.key === 40 && active < searchResults.length - 1){
+      setActive(active + 1)
+    }
+  }
 
 
   const handleSubmit = async(e) => {
@@ -52,6 +61,7 @@ const Search = () => {
         onClick={() => setMenu(true)}
         className="search-input"
         autoFocus
+        onKeyDown={keyDown}
         type="search"
         placeholder="Search"
         onChange={(e) => setSearchVal(e.target.value)}
@@ -63,10 +73,9 @@ const Search = () => {
           {searchResults.length > 0 ? (
             searchResults?.map((ele, i) => {
               return (
-                <NavLink onClick={(() => {setSearchVal('')})} key={i} className="spot-links" to={`/users/${ele.id}`}>
+                <NavLink onClick={(() => {setSearchVal('')})}id={active === i ? 'active' : null}  key={i}  className="spot-links" to={`/users/${ele.id}`}>
                   <div
                     className="search-results-list"
-                    key={i}
                   >
                     {ele.username}
                   </div>
